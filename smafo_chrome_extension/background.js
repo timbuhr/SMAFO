26f2334580b070ddcd13d4b926498db87428997f
1103wojtek <wojtek_go@hotmail.com>
2014-10-15 19:45:53 +0200

    erster Versuch

---
+++ smafo_chrome_extension/background.js
@@ -0,0 +1,15 @@
+/**
+ * @author Wojtek
+ */
+
+
+chrome.app.runtime.onLaunched.addListener(function(){
+
+	
+	chrome.app.window.create('window.html', {
+		'bounds': {
+			'width': 1200,
+      'height': 500
+    }
+  });
+});
