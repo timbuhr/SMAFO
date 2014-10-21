a281c1deaa07810e33f2b82ef349e4ca2e16cbb1
1103wojtek <wojtek_go@hotmail.com>
2014-10-21 23:53:45 +0200

    popup erscheint bei klick

--- smafo_chrome_extension/background.js
+++ smafo_chrome_extension/background.js
@@ -2,14 +2,67 @@
  * @author Wojtek
  */
 
+// chrome.app.runtime.onLaunched.addListener(function(){
+//
+//
+// chrome.app.window.create('window.html', {
+// 'bounds': {
+// 'width': 1200,
+// 'height': 500
+// }
+// });
+// });}
 
-chrome.app.runtime.onLaunched.addListener(function(){
-
-	
-	chrome.app.window.create('window.html', {
-		'bounds': {
-			'width': 1200,
-      'height': 500
-    }
-  });
+chrome.runtime.onInstalled.addListener(function() {
+	var context = "selection";
+	var title = "smafo";
+	var id = chrome.contextMenus.create({
+		"title" : title,
+		"contexts" : [context],
+		"id" : "context" + context
+	});
 });
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+		function onClickHandler(info, tab) {
+		console.log("%s",info.selectionText);
+		
+		//chrome.windows.create();
+	//var sText = info.selectionText;
+	//var url = "https://de.wikipedia.org/wiki/-" + encodeURIComponent(sText);
+
+	//window.open("smafo_window.html","resizable");
+	
+	chrome.windows.create({
+		//'id':'SmaFo_window',
+		'url':'smafo_window.html',
+		'type':'app',
+		'width':300,
+		'height':500
+	});
+
+	// chrome.windows.create("smafo_window.html", {
+	// frame : "none",
+	// id : "framelessWinID",
+	// innerBounds : {
+	// 'width' : 360,
+	// 'height' : 300,
+	// 'left' : 600,
+	// 'minWidth' : 220,
+	// 'minHeight' : 220
+	// },
+	// bounds : {
+	// 'width' : 1200,
+	// 'height' : 500
+// 
+	// }
+	// });
+	// openwindow('smafo_window.html');
+};
+
+// function openwindow(url){
+	  // NewWindow=window.open(url,'newWin','width=300,height=500,left=200,top=200,toolbar=No,location=No,scrollbars=no,status=No,resizable=no,fullscreen=No');  
+	  // NewWindow.focus(); 
+	  // void(0);  
+	  // }
